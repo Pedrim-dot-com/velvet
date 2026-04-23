3a984d83bf1889fd40ea67b6a92f22fb6b25dafc
Pedro Bastos <pedro.bastos1@vodafone.com>
2026-04-23 18:40:01 +0100

    add react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { ContainerWrapper } from './components';
 import Home from './pages/Home';
 
 function App() {
   return (
-    <ContainerWrapper>
-      <Home />
-    </ContainerWrapper>
+    <BrowserRouter>
+      <ContainerWrapper>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </ContainerWrapper>
+    </BrowserRouter>
   );
 }
 
